ca1873f867db240a16b5493f5e03fdfa4290b651
abrshewube <abrshefru1427@gmail.com>
2024-12-13 01:31:45 -0800

    database is added

---
+++ backend/models/vehicleModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const vehicleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Available",
+  },
+  lastUpdated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Vehicle = mongoose.model("Vehicle", vehicleSchema);
+
+module.exports = Vehicle;
